8f2dcecf42d6192db400e31cb4ddda5b11b228f6
Andrew Thomas <apt41790@gmail.com>
2018-06-03 11:56:43 -0400

    Added minimums of 2 of each character type, but I think there might be a better way. Posted to Stack Overflow to see if anyone can help there. https://stackoverflow.com/questions/50668230/how-to-ensure-minimum-occurrences-of-rng-outcomes

--- src/Main.kt
+++ src/Main.kt
@@ -16,11 +16,11 @@ fun main(args: Array<String>) {
 
 }
 
-private fun generatePassword() {
+/*private fun generatePassword() {
 
     var password = ""
 
-    val passwordLength = (8..24).random()
+    val passwordLength = (8..25).random()
 
     for (i in 0..passwordLength) {
         val charType = (1..5).random()
@@ -32,6 +32,69 @@ private fun generatePassword() {
         }
     }
     println(password)
+}*/
+
+private fun generatePassword() {
+
+    val password: ArrayList<Char> = arrayListOf()
+
+    var newChar:Char
+
+    var numLow = 0
+    var numUpp = 0
+    var numDig = 0
+    var numSpe = 0
+
+    while (numLow < 2){
+        newChar = lettersLower[(0..lettersLower.size).random()]
+        password.add((0..password.size).random(), newChar)
+        numLow++
+    }
+    while (numUpp < 2){
+        newChar = lettersUpper[(0..lettersUpper.size).random()]
+        password.add((0..password.size).random(), newChar)
+        numUpp++
+    }
+    while (numDig < 2){
+        newChar = numbers[(0..numbers.size).random()]
+        password.add((0..password.size).random(), newChar)
+        numDig++
+    }
+    while (numSpe < 2){
+        newChar = symbols[(0..symbols.size).random()]
+        password.add((0..password.size).random(), newChar)
+        numSpe++
+    }
+
+    val passwordLength = (8..25).random() - password.size
+
+    for (i in 0..passwordLength) {
+
+
+        val charType = (1..5).random()
+        when (charType) {
+            1 -> {
+                newChar = lettersLower[(0..lettersLower.size).random()]
+                password.add(newChar)
+            }
+            2 -> {
+                newChar = lettersUpper[(0..lettersUpper.size).random()]
+                password.add(newChar)
+            }
+            3 -> {
+                newChar = numbers[(0..numbers.size).random()]
+                password.add(newChar)
+            }
+            4 -> {
+                newChar = symbols[(0..symbols.size).random()]
+                password.add(newChar)
+            }
+        }
+    }
+    for (i in password.indices){
+        print(password[i])
+    }
+    print("\n")
 }
 
 // Random function via user s1m0nw1 on stackoverflow.com (https://stackoverflow.com/a/49507413)
